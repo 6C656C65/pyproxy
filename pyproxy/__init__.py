021ea7af8f790f8ecf35119aaef937252fc8ce1c
6C656C65 <73671374+6C656C65@users.noreply.github.com>
2025-05-31 00:50:17 +0200

    Bump version: 0.3.2 → 0.4.0

--- pyproxy/__init__.py
+++ pyproxy/__init__.py
@@ -5,7 +5,7 @@ that holds the current version number of the application.
 
 import os
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 if os.path.isdir("pyproxy/monitoring"):
     __slim__ = False
